f486b4d7a447538026a7dc65e1a5a874bed017a4
garfield <1955825621@qq.com>
2022-10-05 04:34:15 +0800

    chore: add unplugin-auto-import

--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
-    <Menu ref="menu" :routes="routes" />
+    <SCmenu ref="menu" :routes="routes" />
   </div>
 </template>
 
 <script setup lang="ts">
-import Menu from "/@/components/SCmenu/index.vue";
-import { useRouter } from "vue-router";
-import { Route } from "/#/vue-route";
+import type { Route } from "/#/vue-route";
 const routes = useRouter().options.routes as Route[];
 </script>
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import defineOptions from "unplugin-vue-define-options/vite";
 import Components from "unplugin-vue-components/vite";
+import AutoImport from "unplugin-auto-import/vite";
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 import removeConsole from "vite-plugin-remove-console";
 import { resolve } from "path";
@@ -16,7 +17,13 @@ export default defineConfig({
     defineOptions(),
     Components({
       dts: "./types/components.d.ts",
+      dirs: ["./src/components"],
       resolvers: [AntDesignVueResolver()]
+    }),
+    AutoImport({
+      imports: ["vue", "vue-router"],
+      dts: "./types/auto-imports.d.ts",
+      vueTemplate: true
     })
   ],
   resolve: {
