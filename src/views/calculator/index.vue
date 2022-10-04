689a4615f40a3b61bd001811109a9784f224cdb3
garfield <1955825621@qq.com>
2022-10-05 04:54:35 +0800

    perf: make calculator strongr

--- src/views/calculator/index.vue
+++ src/views/calculator/index.vue
@@ -14,20 +14,36 @@ const resultDom = $ref<HTMLDivElement | null>(null);
 const contentArr = [1, 2, 3, "+", 4, 5, 6, "-", 7, 8, 9, "*", 0, ".", "=", "/"];
 let result = $ref("0");
 let isComputed = $ref(false);
+const isNotOperator = (target: string) =>
+  target !== "+" &&
+  target !== "-" &&
+  target !== "." &&
+  target !== "*" &&
+  target !== "/";
+const isResultEndWidthOperator = () =>
+  result.endsWith("+") ||
+  result.endsWith("-") ||
+  result.endsWith("*") ||
+  result.endsWith("/") ||
+  result.endsWith(".");
 const clickHandler = (e: PointerEvent) => {
   const target = e.target as HTMLDivElement;
   if (target.classList.value !== "item") return;
   const clickContent = target.innerHTML;
   if (clickContent === "=") {
-    result = eval(result);
+    result = eval(result).toString();
     isComputed = true;
-  } else if (isComputed) {
-    result = clickContent;
-    isComputed = false;
   } else if (clickContent === "0") {
     result !== "0" &&
       ((isComputed && (isComputed = false)) || (result += clickContent));
   } else {
+    !isComputed &&
+      clickContent !== "0" &&
+      result === "0" &&
+      isNotOperator(clickContent) &&
+      (result = "");
+    if (!isNotOperator(clickContent) && isResultEndWidthOperator()) return;
+    isComputed && (isComputed = false);
     result += clickContent;
   }
 };
